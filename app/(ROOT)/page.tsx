9a6d556651313b7ff4bfb1dc0c2431af60042cef
Austin Christen <austinacec@gmail.com>
2024-10-01 19:08:17 -0500

    implement Chart JS and animated counter in the balance box

--- app/(ROOT)/page.tsx
+++ app/(ROOT)/page.tsx
@@ -1,8 +1,27 @@
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Austin'}
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className='home-header'>
+          <HeaderBox 
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext='Access and manage your account efficiently.'
+          />
+          <TotalBalanceBox 
+            accounts = {[]}
+            totalBanks = {1}
+            totalCurrentBalance = {1233.75}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
